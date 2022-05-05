5bf02cae7223c4f4509678a8a251f3a0a9627955
Tsyriulnikov <max_djvu@mail.ru>
2022-05-05 09:52:38 +0300

    Updates

---
+++ precache-manifest.4a6b96586e0f9a4af97ac29eb3e351a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f41cd1b7d71b6eee12c09736f97e11",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "723740a56e0ce7c7c238",
+    "url": "/counter/static/css/main.ac05f9e3.chunk.css"
+  },
+  {
+    "revision": "c866f3e6bd4dc095b011",
+    "url": "/counter/static/js/2.acffd382.chunk.js"
+  },
+  {
+    "revision": "6865f50f3618af47c938d1e1cd5951cf",
+    "url": "/counter/static/js/2.acffd382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f084165663e5d26f01ba",
+    "url": "/counter/static/js/3.dc382842.chunk.js"
+  },
+  {
+    "revision": "723740a56e0ce7c7c238",
+    "url": "/counter/static/js/main.7d9b7957.chunk.js"
+  },
+  {
+    "revision": "10787f8e35550a91472d",
+    "url": "/counter/static/js/runtime-main.b34955b7.js"
+  }
+]);
